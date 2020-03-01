3a785e827c540680d104b58442b2a965faaf04db
Lui <Soler>
2020-03-01 10:09:18 -0500

    ButterKinfe implementation

--- app/src/main/java/com/example/calcuapp/MainActivity.java
+++ app/src/main/java/com/example/calcuapp/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.calcuapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_view) Button textview;
+    @BindView(R.id.btn_cero) Button cero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+
+
+
+
     }
 }
